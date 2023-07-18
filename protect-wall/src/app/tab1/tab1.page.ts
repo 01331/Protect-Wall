b74aa62070bc87c8c66baae1dbf9035b10faec72
01331 <20-01331@g.batstate-u.edu.ph>
2023-07-18 15:21:10 +0800

    Updated features

--- protect-wall/src/app/tab1/tab1.page.ts
+++ protect-wall/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,67 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public selectedCondition: string = 'manual'
+  public selectedMethod: string = 'none'
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+
+  async presentActionSheet1() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: "Turn on condition",
+      buttons: [
+        {
+          text: 'Manual',
+          handler: () => {
+            this.selectedCondition = 'manual';
+          }
+        },
+        {
+          text: 'Timer',
+          handler: () => {
+            this.selectedCondition = 'timer';
+          }
+          
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+  async presentActionSheet2() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: "Deactivation Method",
+      buttons: [
+        {
+          text: 'None',
+          handler: () => {
+            this.selectedMethod = 'none';
+          }
+        },
+        {
+          text: 'Pin Code',
+          handler: () => {
+            this.selectedMethod = 'pin';
+          }
+        },
+        {
+          text: 'Charger Connections',
+          handler: () => {
+            this.selectedMethod = 'charge';
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 
 }
